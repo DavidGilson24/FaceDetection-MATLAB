77c08c3926439edcd114dc71e39f96edb251aa47
David Gilson <cs@david-gilson.com>
2023-10-18 13:48:02 +0200

    Script modified

--- scripts/faceDetection.m
+++ scripts/faceDetection.m
@@ -1,51 +1,67 @@
-%TEST 1
-
-% clear all;
-% close all;
-% 
-% faceDetector = vision.CascadeObjectDetector();
-% 
-% vid = videoinput('winvideo', 1);
-% vid.ReturnedColorSpace = 'grayscale';
-% vid.FramesPerTrigger = Inf;
-% vid.FrameGrabInterval = 1;  % Grab every frame
-% 
-% videoPlayer = vision.VideoPlayer('Position', [100, 100, 680, 520]);
-% 
-% start(vid);
-% 
-% while isrunning(vid)
-%     frame = getdata(vid, 1, 'uint8'); % Get one frame at a time
-%     
-%     bbox = step(faceDetector, frame);
-%     detectedImg = insertShape(frame, 'Rectangle', bbox, 'LineWidth', 5);
-%     step(videoPlayer, detectedImg);
-%     
-%     if ~isOpen(videoPlayer)
-%         break;
-%     end
-%     
-%     flushdata(vid, 'triggers'); % Clear the data for the next iteration
-% end
-% 
-% stop(vid);
-% delete(vid);
-% release(videoPlayer);
-
-%TEST 2
-
-clear all;
-close all;
-
-faceDetect = vision.CascadeObjectDetector();
+faceDetector = vision.CascadeObjectDetector();
 
 vid = videoinput('winvideo', 1);
-videoFrames = readFrame(vid);
-faceBox = step(faceDetector, videoFrame);
+vid.ReturnedColorSpace = 'grayscale';
+vid.FramesPerTrigger = Inf;
+triggerconfig(vid, 'manual');
+start(vid);
 
-videoFrame = insertShape(videoFrame, "rectangle", faceBox);
-faceBoxArray = faceBox2points(faceBoxbox(1, :));
+trigger(vid);
+videoFrame = getdata(vid);
 
+bbox = step(faceDetector, videoFrame);
+
+videoFrame = insertShape(videoFrame, "rectangle", bbox);
+figure; imshow(videoFrame); title("Detected face");
+
+bboxPoints = bbox2points(bbox(1, :));
+
+points = detectMinEigenFeatures(im2gray(videoFrame), "ROI", bbox);
+
+figure, imshow(videoFrame), hold on, title("Detected features");
+plot(points);
+
+pointTracker = vision.PointTracker("MaxBidirectionalError", 2);
+
+points = points.Location;
+initialize(pointTracker, points, videoFrame);
+
+videoPlayer  = vision.VideoPlayer("Position",...
+    [100 100 [size(videoFrame, 2), size(videoFrame, 1)]+30]);
+
+oldPoints = points;
+
+% Use a loop to continuously capture frames from the webcam
+while ishandle(videoPlayer.Parent)
+    trigger(vid);
+    videoFrame = getdata(vid);
+
+    % Track the points. Note that some points may be lost.
+    [points, isFound] = step(pointTracker, videoFrame);
+    visiblePoints = points(isFound, :);
+    oldInliers = oldPoints(isFound, :);
+    
+    if size(visiblePoints, 1) >= 2 % need at least 2 points
+        [xform, inlierIdx] = estimateGeometricTransform2D(...
+            oldInliers, visiblePoints, "similarity", "MaxDistance", 4);
+        oldInliers    = oldInliers(inlierIdx, :);
+        visiblePoints = visiblePoints(inlierIdx, :);
+        
+        bboxPoints = transformPointsForward(xform, bboxPoints);
+        bboxPolygon = reshape(bboxPoints', 1, []);
+        videoFrame = insertShape(videoFrame, "polygon", bboxPolygon, "LineWidth", 2);
+        videoFrame = insertMarker(videoFrame, visiblePoints, "+", "MarkerColor", "white");       
+        oldPoints = visiblePoints;
+        setPoints(pointTracker, oldPoints);        
+    end
+    
+    step(videoPlayer, videoFrame);
+end
+
+% Clean up
+stop(vid);
+delete(vid);
+release(videoPlayer);
 
 
     
